446703eaa4e4100d32f8fbf42e485a8a5a6a74f8
Evan <esamanoaz@gmail.com>
2023-07-23 16:45:23 -0700

    Followed TechWithTim tutorial
    I followed a Tech with Tim tutorial on python sockets. Right now there is a server that listens on a port for connections and messages and handles clients disconnecting. Clients can connect to the server if they have the address and port and can send messages. Client can receive a 'seen' confirmation message from the server.

--- client.py
+++ client.py
@@ -2,3 +2,35 @@
 # Date:     July 22nd, 2023
 # Version:  1.0
 # Desc:     A client to play the card game 'Auction' over a network.
+# yt-tutorial: https://www.youtube.com/watch?v=3QiPPX-KeSc
+
+
+from auction import play
+from socket import *
+from threading import Thread, activeCount
+
+HEADER = 4
+PORT = 25000
+IP = gethostbyname(gethostname())
+ADDR = (IP, PORT)
+FORMAT = 'utf-8'
+DISCONN_MSG = '!DISCONNECT'
+
+client = socket(AF_INET, SOCK_STREAM)
+client.connect(ADDR)
+
+def send(_msg):
+    # note: the purpose of the header is to indicate how long the following msg will be
+    msg = _msg.encode(FORMAT)
+    msg_len = len(msg)
+    send_len = str(msg_len).encode(FORMAT)
+    send_len += b' ' * (HEADER - len(send_len))
+    client.send(send_len) # msg header
+    client.send(msg) # msg body
+    print(client.recv(12).decode(FORMAT))
+
+
+if __name__ == '__main__':
+    send("hello howdy pal you the best!!!! :D")
+    send(input('[ENTER MSG]: '))
+    send(DISCONN_MSG)
--- server.py
+++ server.py
@@ -2,8 +2,54 @@
 # Date:     July 22nd, 2023
 # Version:  1.0
 # Desc:     A server to host the card game 'Auction' over a network.
+# yt-tutorial: https://www.youtube.com/watch?v=3QiPPX-KeSc
+
 
 from auction import play
+from socket import *
+from threading import Thread, active_count
+
+HEADER = 4
+PORT = 25000
+IP = gethostbyname(gethostname())
+ADDR = (IP, PORT)
+FORMAT = 'utf-8'
+DISCONN_MSG = '!DISCONNECT'
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    print(f'[NEW CONNECTION] {addr} connected.')
+
+    connected = True
+    while connected:
+        if msg_len := conn.recv(HEADER).decode(FORMAT): # if we receive a msg
+            msg_len = int(msg_len) # then store the header (first msg) as the length of the next msg
+            msg = conn.recv(msg_len).decode(FORMAT) # then listen for the body (second msg) since we know its length
+            if msg == DISCONN_MSG:
+                connected = False
+            
+            print(f'[{addr}] {msg}')
+            conn.send('MSG received'.encode(FORMAT))
+    
+    conn.close()
+
+
+def start():
+    sock.listen()
+    print(f'Server is listening on {IP}:{PORT}')
+    while True:
+        conn, addr = sock.accept()
+        thread = Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f'[ACTIVE CONNECTIONS] {active_count() - 1}')
+
+
+if __name__ == '__main__':
+    print('[STARTING] Server is starting now...')
+    start()
 
 
 # establish socket opening on port
