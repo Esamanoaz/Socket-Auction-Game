55a5ba268b7f98ad342dc23dd3960a3c055b16af
Evan <esamanoaz@gmail.com>
2023-07-23 18:18:56 -0700

    Setup input stuff and thread safe print function

--- client.py
+++ client.py
@@ -2,22 +2,15 @@
 # Date:     July 22nd, 2023
 # Version:  1.0
 # Desc:     A client to play the card game 'Auction' over a network.
-# yt-tutorial: https://www.youtube.com/watch?v=3QiPPX-KeSc
+#           yt-tutorial: https://www.youtube.com/watch?v=3QiPPX-KeSc
 
 
-from auction import play
 from socket import *
-from threading import Thread, activeCount
+#from threading import Thread
 
 HEADER = 4
-PORT = 25000
-IP = gethostbyname(gethostname())
-ADDR = (IP, PORT)
 FORMAT = 'utf-8'
 DISCONN_MSG = '!DISCONNECT'
-
-client = socket(AF_INET, SOCK_STREAM)
-client.connect(ADDR)
 
 def send(_msg):
     # note: the purpose of the header is to indicate how long the following msg will be
@@ -29,8 +22,23 @@ def send(_msg):
     client.send(msg) # msg body
     print(client.recv(12).decode(FORMAT))
 
-
 if __name__ == '__main__':
-    send("hello howdy pal you the best!!!! :D")
-    send(input('[ENTER MSG]: '))
-    send(DISCONN_MSG)
+    setup = input('[SETUP] Default setup? (y/n): ')
+    if setup.lower() == 'n':
+        IP = int(input('[IP]: '))
+        PORT = int(input('[PORT]: '))
+    else:
+        IP = gethostbyname(gethostname())
+        PORT = 25000
+        print(f'[SETUP] Set up by default to {IP}:{PORT}')
+    
+    ADDR = (IP, PORT)
+    client = socket(AF_INET, SOCK_STREAM)
+    client.connect(ADDR)
+
+    while connected := True:
+        msg = input('[ENTER MSG]: ')
+        if msg == DISCONN_MSG:
+            send(msg)
+            break
+        send(msg)
--- server.py
+++ server.py
@@ -2,12 +2,18 @@
 # Date:     July 22nd, 2023
 # Version:  1.0
 # Desc:     A server to host the card game 'Auction' over a network.
-# yt-tutorial: https://www.youtube.com/watch?v=3QiPPX-KeSc
+#           yt-tutorial: https://www.youtube.com/watch?v=3QiPPX-KeSc
 
+# this server needs to:
+#   startup and receive client connections
+#   first client to connect is Party Leader
+#   Party Leader can !START the game
+#   After !START is called, check if there are more than 1 and less than 5 clients
+#   Pass usernames in and create Player objects to start game using auction.play
 
-from auction import play
+from auction import play, Player
 from socket import *
-from threading import Thread, active_count
+from threading import Thread, active_count, Lock
 
 HEADER = 4
 PORT = 25000
@@ -15,9 +21,17 @@ IP = gethostbyname(gethostname())
 ADDR = (IP, PORT)
 FORMAT = 'utf-8'
 DISCONN_MSG = '!DISCONNECT'
+LOCK = Lock()
 
 sock = socket(AF_INET, SOCK_STREAM)
 sock.bind(ADDR)
+
+
+def safe_print(*a, **b):
+    '''Thread safe print function
+    https://bramcohen.livejournal.com/70686.html'''
+    with LOCK:
+        print(*a, **b)
 
 
 def handle_client(conn, addr):
@@ -31,7 +45,7 @@ def handle_client(conn, addr):
             if msg == DISCONN_MSG:
                 connected = False
             
-            print(f'[{addr}] {msg}')
+            safe_print(f'[{addr}] {msg}')
             conn.send('MSG received'.encode(FORMAT))
     
     conn.close()
@@ -46,8 +60,16 @@ def start():
         thread.start()
         print(f'[ACTIVE CONNECTIONS] {active_count() - 1}')
 
-
 if __name__ == '__main__':
+    IP = gethostbyname(gethostname())
+    setup = input('[SETUP] Set port to 25000? (y/n): ')
+    if setup.lower() == 'n':
+        PORT = int(input('[PORT]: '))
+    else:
+        PORT = 25000
+        print(f'[SETUP] Set up by default to {IP}:{PORT}')
+    ADDR = (IP, PORT)
+
     print('[STARTING] Server is starting now...')
     start()
 
